61b8695f2f167e8849a15e377abf90d653725914
bk <litlpoet@gmail.com>
2015-07-30 02:47:44 +0900

    Compilation on linux. fixing .dir-locals.el

---
+++ .dir-locals-linux.el
@@ -0,0 +1,9 @@
+((nil
+  .
+  ((projectile-project-compilation-cmd
+    . "make -j 4 -C \"~/VersionControl/Research/libML/build\"")
+   (projectile-project-run-cmd
+    . "make test -C \"~/VersionControl/Research/libML/build\"")
+   )
+  )
+ )
--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,1 @@
-((nil
-  .
-  ((projectile-project-compilation-cmd
-    . "make -j 4 -C \"s:/VersionControl/Research/libML/build\"")
-   (projectile-project-run-cmd
-    . "make test -C \"s:/VersionControl/Research/libML/build\"")
-   )
-  )
- )
+.dir-locals-linux.el
