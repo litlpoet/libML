4ed30219b6ca425be554d5f2e2571b0178db73ef
Byungkuk Choi <litlpoet@gmail.com>
2015-08-13 12:10:36 +0900

    fix .dir-locals.el

--- .dir-locals-linux.el
+++
@@ -1,9 +0,0 @@
-((nil
-  .
-  ((projectile-project-compilation-cmd
-    . "make -j 4 -C \"~/VersionControl/Research/libML/build\"")
-   (projectile-project-run-cmd
-    . "make test -C \"~/VersionControl/Research/libML/build\"")
-   )
-  )
- )
--- .dir-locals-win.el
+++
@@ -1,9 +0,0 @@
-((nil
-  .
-  ((projectile-project-compilation-cmd
-    . "make -j 4 -C \"s:/VersionControl/Research/libML/build\"")
-   (projectile-project-run-cmd
-    . "make test -C \"s:/VersionControl/Research/libML/build\"")
-   )
-  )
- )
